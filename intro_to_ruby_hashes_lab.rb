d7c9b871cce6101cb4e863b429839ff1ecf8de89
Jacob Viti <thevitij64@gmail.com>
2020-07-17 04:54:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,14 +24,20 @@ def monopoly_with_second_tier
   monopoly =  {
     :railroads=>
      {
-       :pieces=>4,
+       :pieces=>{
+         
+       }
        :rent_in_dollars=>{
 
        }
-       :names=>
-    {
+       :names=>{
+    
 
-    }
+       }
+       :shortline=>
+       {
+      
+      }
      }
   }
 end
